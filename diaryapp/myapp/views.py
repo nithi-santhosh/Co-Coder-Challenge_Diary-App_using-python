3cbe4e3e26218dbdd682b88331f5262e4b9c2cba
Neeraj <neerajbabu27@gmail.com>
2022-03-01 15:09:25 +0530

    Added routings,models and dynamic card rendering
    Added routings to add and diary pages, made modelto store diary data and added dynamic rendering to home page to render cards using data from model.

--- diaryapp/myapp/views.py
+++ diaryapp/myapp/views.py
@@ -1,5 +1,21 @@
+from dataclasses import dataclass
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Data
+
+
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    data1 = Data()
+    data1.id = 0
+    data1.heading = "head1"
+    data1.body = "body1"
+    data1.time = "2/2/2022 1:32 PM"
+    datas = [data1]
+    return render(request,'index.html',{'datas' : datas})
+
+def diary(request):
+    return render(request,'diary.html')
+
+def add(request):
+    return render(request,'add.html')
